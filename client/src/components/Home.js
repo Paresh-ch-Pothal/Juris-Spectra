43af8c81cd944b4e8764edfadb120608ac3ff81c
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-07-18 14:14:50 +0530

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="homepage">
+        <div className="right"></div>
+        <div className="left"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
