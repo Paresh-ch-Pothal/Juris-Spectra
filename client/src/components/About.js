f515d160d486f479636a570d914892567bd2de00
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-07-23 12:58:26 +0530

    updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,12 +6,12 @@ const About = () => {
     <>
       <div className="outer" >
         <h1 className='pric'>About us</h1>
-        <p className="box">Hustling Learning is a platform dedicated to providing valuable educational resources and content. We focus on offering practical insights and strategies to help individuals enhance their skills and knowledge in various fields. Our mission is to empower learners to achieve their personal and professional goals through accessible and engaging materials. Whether you're looking to improve your career prospects, acquire new skills, or simply expand your knowledge, Hustling Learning is here to support your journey.</p>
+        <p className="box" style={{marginBottom: "40px"}}>Hustling Learning is a platform dedicated to providing valuable educational resources and content. We focus on offering practical insights and strategies to help individuals enhance their skills and knowledge in various fields. Our mission is to empower learners to achieve their personal and professional goals through accessible and engaging materials. Whether you're looking to improve your career prospects, acquire new skills, or simply expand your knowledge, Hustling Learning is here to support your journey.</p>
 
-        <div className="founder flex">
+        <div className="founder flex"b style={{marginBottom: "40px"}}>
           <div className="leftabout">
             <div><img src="" alt="founder" srcset="" />
-              <p>Lorem ipsum dolor sit amet.</p>
+              <p>Lorem ipsum dolor sit ametd Lorem ipsum,.</p>
             </div>
           </div>
           <div className="rightabout">
